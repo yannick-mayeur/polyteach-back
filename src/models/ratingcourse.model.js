2391cf701ab6655ccb9850c44fd9a82ed0991c9f
elmadanimarouane <marouane.el-madani@etu.umontpellier.fr>
2019-11-14 00:27:17 +0100

    correcting RatingCourse.create and returning result when done

--- src/models/ratingcourse.model.js
+++ src/models/ratingcourse.model.js
@@ -1,15 +1,18 @@
 const db = require('../db');
+const logger = require('../helpers/logger');
 
 const RatingCourse = {
   async create(obj) {
-    const text = 'INSERT INTO ratingcourse VALUES($1, $2, $3)';
+    const text = 'INSERT INTO ratingcourse(`iduser-ratingcourse`, `idcourse-ratingcourse`, `value-ratingcourse`) VALUES($1, $2, $3) RETURNING *;';
     const values = [obj.idUserRatingCourse, obj.idCourseRatingCourse, obj.valueRatingCourse];
-    return db.query(text, values)
-      .then(res => res)
-      .catch(e => {
-        console.log(e.stack);
-        throw new Error('error creating a ratingcourse');
-      });
+    try {
+      const resultRatingCourse = await db.query(text,values);
+      return resultRatingCourse.rows[0];
+    }
+    catch (e) {
+      logger.log('RatingCourse.create: ' + e.stack);
+      throw new Error('error in creating a ratingcourse');
+    }
   }
 };
 
--- src/routes/ratingcourse.route.js
+++ src/routes/ratingcourse.route.js
@@ -3,7 +3,7 @@ const M = require('../models');
 module.exports = (router) => {
   router.post('/ratingcourse', async (req, res) => {
     M.RatingCourse.create(req.body)
-      .then(() => res.sendStatus(200))
+      .then((ratingcourse) => res.status(200).send(ratingcourse))
       .catch((err) => {
         res.statusMessage = err;
         res.sendStatus(500);
