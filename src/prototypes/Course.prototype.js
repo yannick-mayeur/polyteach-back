8a3c2a191dd8726b4e53d303b4615dd6eb9106b7
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-13 09:17:10 +0100

    Fix case on name of course attributes

--- src/prototypes/Course.prototype.js
+++ src/prototypes/Course.prototype.js
@@ -1,13 +1,13 @@
 module.exports = class Course {
-  constructor(idcourse, namecourse, descriptioncourse, picturecourse) {
-    this.id = idcourse;
-    this.name = namecourse;
-    this.description = descriptioncourse;
-    this.picture = picturecourse;
+  constructor(idCourse, nameCourse, descriptionCourse, pictureCourse) {
+    this.id = idCourse;
+    this.name = nameCourse;
+    this.description = descriptionCourse;
+    this.picture = pictureCourse;
   }
 
   static dbToCourse(obj) {
-    const course = new Course(obj.idcourse, obj.namecourse, obj.descriptioncourse, obj.picturecourse);
+    const course = new Course(obj.idCourse, obj.nameCourse, obj.descriptionCourse, obj.pictureCourse);
     return course;
   }
 
