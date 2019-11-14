47e014699abb0ce843cb549a44e1e85599510651
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-14 22:42:47 +0100

    Update course and video creation

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,16 +1,22 @@
 const db = require('../db');
+const logger = require('../helpers/logger');
 // const P = require('../prototypes');
 
 const Video = {
   async create(obj) {
-    const text = 'INSERT INTO video("idVideo", "titleVideo") VALUES(DEFAULT, $1) RETURNING *;';
-    const values = [obj.title]; //, obj.fk_course];
-    return db.query(text, values)
-      .then(res => res.rows[0])
-      .catch(e => {
-        console.log(e.stack);
-        throw new Error('error video create');
-      });
+    logger.log('info', 'video.model.create called with:', obj);
+    const text = 'INSERT INTO video(idvideo, titlevideo, hashserver, hashvtt, "idchapter-video") \
+      VALUES(DEFAULT, $1, $2, $3, $4) RETURNING *;';
+    const values = [obj.title, obj.videoURL, obj.vttURL, obj.fk_course];
+    try {
+      const resVideo = await db.query(text, values);
+      const res = resVideo.rows[0];
+      logger.log('info', 'video.model.create returning:', res);
+      return res;
+    } catch(e) {
+      logger.error(e.stack);
+      throw new Error('error video create');
+    }
   }
 };
 
