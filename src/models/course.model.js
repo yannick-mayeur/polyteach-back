94ece7849f349c254e639271524cc5c8493accc8
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-14 22:42:47 +0100

    Update course and video creation

--- src/models/course.model.js
+++ src/models/course.model.js
@@ -10,23 +10,34 @@ const Course = {
     return db.query(query, [])
       .then(({ rows }) => P.Course.dbToCourses(rows))
       .catch((e) => {
-        logger.error('Course.getAll: ' + e.stack);
+        logger.log('error', 'Course.getAll', e);
         throw new Error('error course getAll');
       });
   },
 
   async create(obj) {
-    const text = 'INSERT INTO course("idCourse", "nameCourse", "descriptionCourse", "pictureCourse") \
+    const text = 'INSERT INTO course(idcourse, namecourse, descriptioncourse, picturecourse) \
                   VALUES(DEFAULT, $1, $2, $3) RETURNING *;';
     const values = [obj.name, obj.description, obj.picture];
     try {
       const resCourse = await db.query(text,values);
-      obj.videos.forEach(async video => {
-        await Video.create({ title: video.title, fk_course: resCourse.rows[0].idcourse });
-      });
-      return resCourse.rows[0];
+      let res = resCourse.rows[0];
+      if (Array.isArray(obj.videos)) {
+        const resVideo = await Promise.all(obj.videos.map(video => {
+          return Video.create({
+            title: video.titleVideo,
+            videoURL: video.videoURL,
+            vttURL: video.vttURL,
+            fk_course: resCourse.rows[0].idcourse
+
+          });
+        }));
+        res.videos = resVideo;
+      }
+      logger.log('info', 'course.model.create returning:', res);
+      return res;
     } catch(e) {
-      logger.error('Course.create(' + obj + '): ' + e.stack);
+      logger.log('error', 'Course.create(' + obj + '): ' + e);
       throw new Error('error in course.model.create');
     }
   }
