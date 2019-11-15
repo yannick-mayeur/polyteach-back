affed289debe5c366f47c5559c3af1154d536bb8
Théo <theo.ponthieu@hotmail.fr>
2019-11-15 15:48:59 +0100

    Add signin and signup method

--- src/models/login.model.js
+++ src/models/login.model.js
@@ -10,27 +10,80 @@ const myPlaintextPassword = 'c';
 const someOtherPlaintextPassword = 'not_bacon';
 
 const login = {
+
+    /**
+     * Return the student or teacher with rigth credentials
+     * @param {String} email of student or teacher
+     * @param {String} password of student or teacher
+     */
+    async login(email, password) {
+        if (email.endsWith("etu.umontpellier.fr")) {
+            return this.loginStudent(email, password)
+        } else if (email.endsWith("umontpellier.fr")) {
+            return this.loginTeacher(email, password)
+        } else {
+            throw new Error("Your are not allowed to connect with this email.")
+        }
+    },
+
     /**
      * If student provide rigth credentials, throw an error instead
      * @param {String} email of the student who want to connect
      * @param {String} password of the student 
      */
-    async login(email, password) {
-        const query = 'SELECT * FROM TEACHER where "emailTeacher"=$1';
+    async loginTeacher(email, password) {
+        const query = 'SELECT * FROM TEACHER where emailteacher=$1';
         return db.query(query, [email])
             .then(({ rows }) => {
                 if (rows.length > 0) {
                     const teacher = P.Teacher.dbToTeacher(rows[0])
-                    if (bcrypt.compareSync(password, teacher.password)) {
+                    if (bcrypt.compareSync(password, rows[0].passwordteacher)) {
                         const token = generateToken(teacher.id);
                         return { user: teacher, token: token }
+                    }
+
+                }
+                throw new Error('Login or password are wrong.')
+            })
+    },
+
+    /**
+     * If student provide rigth credentials, throw an error instead
+     * @param {String} email of the student who want to connect
+     * @param {String} password of the student 
+     */
+    async loginStudent(email, password) {
+        const query = 'SELECT * FROM STUDENT where emailstudent=$1';
+        return db.query(query, [email])
+            .then(({ rows }) => {
+                if (rows.length > 0) {
+                    if (email.endsWith("@etu.umontpellier.fr")) {
+                        const student = P.Student.dbToStudent(rows[0])
+                        if (bcrypt.compareSync(password, rows[0].passwordstudent)) {
+                            const token = generateToken(student.id);
+                            return { user: student, token: token }
+                        }
                     }
                 }
                 throw new Error('Login or password are wrong.')
             })
     },
 
+    async isEmailAlreadyUsed(email) {
+        const q = `(SELECT emailteacher FROM public.teacher where emailteacher=$1) union 
+       (SELECT emailstudent FROM public.student where emailstudent=$1);`;
+        return db.query(q, [email])
+            .then(({ rows }) => {
+                return rows.length > 0;
+            })
+            .catch(err => {
+                console.log(err);
+                throw new Error('Error login.model email already user');
+            });
+    },
+
     async signupStudent(email, password, firstName, lastName, classStudent) {
+
         const query = 'INSERT INTO student values(DEFAULT, $1, $2, $3, $4, $5, $6) RETURNING *';
 
         // hash password
--- src/prototypes/Teacher.prototype.js
+++ src/prototypes/Teacher.prototype.js
@@ -1,15 +1,14 @@
 module.exports = class Teacher {
-    constructor(idTeacher, emailTeacher, passwordTeacher, roleTeacher, firstNameTeacher, lastNameTeacher) {
+    constructor(idTeacher, emailTeacher, roleTeacher, firstNameTeacher, lastNameTeacher) {
       this.id = idTeacher;
       this.email = emailTeacher;
-      this.password = passwordTeacher;
       this.role = roleTeacher;
       this.firstName = firstNameTeacher;
       this.lastName = lastNameTeacher;
     }
   
     static dbToTeacher(obj) {
-      const course = new Teacher(obj.idTeacher, obj.emailTeacher, obj.passwordTeacher, obj.roleTeacher, obj.firstNameTeacher, obj.lastNameTeacher);
+      const course = new Teacher(obj.idTeacher, obj.emailTeacher, obj.roleTeacher, obj.firstNameTeacher, obj.lastNameTeacher);
       return course;
     }
   
