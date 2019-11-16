6788f76cf6afec23756014c430202683a2100773
Théo <theo.ponthieu@hotmail.fr>
2019-11-16 17:36:08 +0100

    Passage au lint

--- src/prototypes/Student.prototype.js
+++ src/prototypes/Student.prototype.js
@@ -1,26 +1,25 @@
 module.exports = class Student {
-    constructor(idStudent, emailStudent, roleStudent, firstNameStudent, lastNameStudent, classStudent) {
-      this.id = idStudent;
-      this.email = emailStudent;
-      this.role = roleStudent;
-      this.firstName = firstNameStudent;
-      this.lastName = lastNameStudent;
-      this.class = classStudent;
-    }
-  
-    static dbToStudent(obj) {
-      const course = new Student(obj.idStudent, obj.emailStudent, obj.roleStudent, obj.firstNameStudent, obj.lastNameStudent, obj.classStudent);
-      return course;
-    }
-  
-    static dbToStudents (tabObjs) {
-        let tabStudents = []
-        tabObjs.forEach(obj => {
-            tabStudents.push(this.dbToStudent(obj));
-        });
-        return tabStudents
-    }
-  
-  };
+  constructor(idStudent, emailStudent, roleStudent, firstNameStudent, lastNameStudent, classStudent) {
+    this.id = idStudent;
+    this.email = emailStudent;
+    this.role = roleStudent;
+    this.firstName = firstNameStudent;
+    this.lastName = lastNameStudent;
+    this.class = classStudent;
+  }
 
-  
+  static dbToStudent(obj) {
+    const course = new Student(obj.idStudent, obj.emailStudent, obj.roleStudent, obj.firstNameStudent, obj.lastNameStudent, obj.classStudent);
+    return course;
+  }
+
+  static dbToStudents(tabObjs) {
+    let tabStudents = [];
+    tabObjs.forEach(obj => {
+      tabStudents.push(this.dbToStudent(obj));
+    });
+    return tabStudents;
+  }
+
+};
+
