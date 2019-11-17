6092d90c19f1c4220a32e1a458c406d9b4980366
Théo <theo.ponthieu@hotmail.fr>
2019-11-18 00:20:27 +0100

    Rules eslint ok

--- src/services/umMail.service.js
+++ src/services/umMail.service.js
@@ -3,10 +3,10 @@
  * firstname.lastname@foo.foo
  * @param {String} email to extract
  */
-const extractNameFromEmail = function(email) {
-    const fandl = email.split("@")[0];
-    const [firstname, lastname] = fandl.split('.');
-    return {firstname, lastname}
-}
+const extractNameFromEmail = function (email) {
+  const fandl = email.split('@')[0];
+  const [firstname, lastname] = fandl.split('.');
+  return { firstname, lastname };
+};
 
-module.exports = { extractNameFromEmail }
+module.exports = { extractNameFromEmail };
