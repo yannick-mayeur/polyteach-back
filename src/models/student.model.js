b2b386a7cac1d20a8ad446f0b305316157d83876
Yannick Mayeur <yannick.mayeur@gmail.com>
2019-11-18 15:16:03 +0100

    Add getAll for students

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -1,7 +1,19 @@
 const db = require('../db');
 const P = require('../prototypes');
+const logger = require('../helpers/logger');
 
 const Student = {
+  async getAll() {
+    logger.info('Student.getAll called');
+    const query = 'SELECT * FROM student;';
+    return db.query(query, [])
+      .then(({ rows }) => {return P.Student.dbToStudents(rows);})
+      .catch((e) => {
+        logger.log('error', 'Student.getAll', e);
+        throw new Error('error student getAll');
+      });
+  },
+
   /**
    * Return the student prototype if name match, undefined instead.
    * @param {String} firsname of the student
