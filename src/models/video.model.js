bc99b86a69af5056e1939d9697a54ba545f44af1
Alexis Sanchez <alexis559.sanchez@gmail.com>
2019-11-18 20:27:42 +0100

    [Done] Add video route to create a video in the DB and Get a video by its name.

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,6 @@
 const db = require('../db');
 const logger = require('../helpers/logger');
-// const P = require('../prototypes');
+const P = require('../prototypes');
 
 const Video = {
   async create(obj) {
@@ -17,7 +17,16 @@ const Video = {
       logger.error(e.stack);
       throw new Error('error video create');
     }
-  }
+  },
+  async getVideoByName(videoName) {
+    const q = `SELECT * FROM video v WHERE v.titlevideo = $1;`;
+    return db.query(query, [videoName])
+      .then(({ rows }) => {return P.Video.dbToVideo(rows[0]);})
+      .catch((e) => {
+        logger.log('error', 'Video.getVideoByName', e);
+        throw new Error('error video getVideoByName');
+      });
+  },
 };
 
 module.exports = Video;
