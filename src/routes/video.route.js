a77c2886a4dd643463f0de8488b685266040dc1b
Alexis Sanchez <alexis559.sanchez@gmail.com>
2019-11-18 21:34:03 +0100

    Fix problems for video route.

--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -2,18 +2,18 @@ const M = require('../models');
 const logger = require('../helpers/logger');
 
 module.exports = (router) => {
-  router.post('/video', async (req, res) => {
-    logger.log('info', 'received request: POST /video\nbody:', req.body);
+  router.post('/videos', async (req, res) => {
+    logger.log('info', 'received request: POST /videos\nbody:', req.body);
     M.Video.create(req.body)
       .then((video) => res.status(200).send(video))
       .catch((err) => {
-        logger.log('error', 'POST /video failed', err);
+        logger.log('error', 'POST /videos failed', err);
         res.sendStatus(500);
       });
   });
 
-  router.get('/video/getByName', async (req, res) => {
-    M.Video.getVideoByName(req.video.videoName).then((video) => {
+  router.get('/video/:videoName', async (req, res) => {
+    M.Video.getVideoByName(req.params.videoName).then((video) => {
       res.status(200).send(video);
     })
       .catch(err => {
