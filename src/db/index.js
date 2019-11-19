f3b7324626265f9eec2ef0792cc2f672684efa0d
Anissa Lamh <anissa.lamhaouar@gmail.com>
2019-11-20 00:34:01 +0100

    model for saveLive after its creation

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,13 @@
 const { Pool } = require('pg');
-const pool = new Pool();
+const pool = new Pool(
+  {
+    user: 'anissalahm',
+    host: 'localhost',
+    database: 'polyteach',
+    port:'5432,'
+
+  }
+);
 
 module.exports = {
   query: (text, params) => {
--- src/models/live.model.js
+++ src/models/live.model.js
@@ -1,8 +1,9 @@
+const db = require('../db');
+
 const Live = {
-  
     async create(obj) {
-      const text = 'INSERT INTO live VALUES(DEFAULT, $1)';
-      const values = [obj.createdAt];
+      const text = 'INSERT INTO live VALUES($1, $2, $3, $4,, $5, $6,$7)';
+      const values = [obj.idSession,obj.nameSession,obj.nameteacher,obj.descriptionlive,obj.timestartlive,/*obj.timestoplive*/];
       return db.query(text, values)
         .then(res => res)
         .catch(e => {
