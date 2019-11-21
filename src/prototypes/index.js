57525d842b4c6ddb15bf937ef776ec3af6ca7886
Théo <theo.ponthieu@hotmail.fr>
2019-11-21 19:08:00 +0100

    remove old imports

--- src/prototypes/index.js
+++ src/prototypes/index.js
@@ -2,12 +2,10 @@ const Course = require('./Course.prototype');
 const Video = require('./Video.prototype');
 const Student = require('./Student.prototype');
 const Teacher = require('./Teacher.prototype');
-const Possescourse = require('./Possescourse.prototype');
 
 module.exports = {
   Course, 
   Video,
   Student,
   Teacher,
-  Possescourse,
 };
