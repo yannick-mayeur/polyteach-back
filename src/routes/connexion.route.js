f0806c8ecae98d797b91ee1994f5fd4c560e105f
Théo <theo.ponthieu@hotmail.fr>
2019-11-21 17:27:05 +0100

    Delete inutile fonction et des betises

--- src/routes/connexion.route.js
+++ src/routes/connexion.route.js
@@ -15,39 +15,46 @@ module.exports = (router) => {
   router.post('/signup', async (req, res) => {
     const token = req.body.token;
     if (T.isTokenValid(token)) {
-      const decodedToken = T.decryptToken(token);
-      const user = {
-        email: decodedToken.email,
-        firstname: decodedToken.firstname,
-        lastname: decodedToken.lastname,
-        role: decodedToken.role,
-        section: decodedToken.section
-      };
-      const isEmailAlreadyUsed = await M.Login.isEmailAlreadyUsed(decodedToken.email);
-      if (isEmailAlreadyUsed) {
-        res.statusMessage = 'This email is already used.';
-        res.status(200).send(user);
-      } else {
-        if (decodedToken.role == 'student') {
-          M.Login.signupStudent(user.email, user.firstname, user.lastname, user.section.toUpperCase()).then(() => {
-            res.status(200).send(user);
-          }).catch(err => {
-            console.log(err);
-            res.statusMessage = err;
-            res.status(500).send();
-          });
-        } else if (decodedToken.role == 'teacher') {
-          M.Login.signupTeacher(user.email, user.firstname, user.lastname).then(() => {
-            res.status(200).send(user);
-          }).catch(err => {
-            console.log(err);
-            res.statusMessage = err;
-            res.status(500).send();
-          });
+      try {
+        const decodedToken = T.decryptToken(token);
+        console.log(decodedToken)
+        const user = {
+          email: decodedToken.email,
+          firstname: decodedToken.firstname,
+          lastname: decodedToken.lastname,
+          role: decodedToken.role,
+          section: decodedToken.section
+        };
+        const isEmailAlreadyUsed = await M.Login.isEmailAlreadyUsed(decodedToken.email);
+        if (isEmailAlreadyUsed) {
+          res.statusMessage = 'This email is already used.';
+          res.status(200).send(user);
         } else {
-          res.status(401).send();
+          if (decodedToken.role == 'student') {
+            M.Login.signupStudent(user.email, user.firstname, user.lastname, user.section.toUpperCase()).then(() => {
+              res.status(200).send(user);
+            }).catch(err => {
+              console.log(err);
+              res.statusMessage = err;
+              res.status(500).send();
+            });
+          } else if (decodedToken.role == 'teacher') {
+            M.Login.signupTeacher(user.email, user.firstname, user.lastname).then(() => {
+              res.status(200).send(user);
+            }).catch(err => {
+              console.log(err);
+              res.statusMessage = err;
+              res.status(500).send();
+            });
+          } else {
+            res.status(401).send();
+          }
         }
+      } catch(err) {
+        res.statusMessage = err;
+        res.status(401).send();
       }
+      
     } else {
       res.status(401).send();
     }
